641db162830f2b48cfbdeddb7f1402832e32cb6f
Fahmi Zain <fahmizain000@gmail.com>
2024-03-14 16:22:57 +0700

    add stock data reducer

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loginSlice from "./loginSlice";
+import dataSlice from "./DataSlice";
 
 const store = configureStore({
   reducer: {
-    login: loginSlice.reducer
+    login: loginSlice.reducer,
+    stockData: dataSlice.reducer
   }
 })
 
